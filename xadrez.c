8b277aad1b5a2ef3f335e2cbae8550f1ebcd17c9
L. Felipe G. Fernandes <70975952+EuLipao@users.noreply.github.com>
2026-07-19 17:55:27 +0000

    feito os menus de escolha para qual lado quer ir

--- xadrez.c
+++ xadrez.c
@@ -7,7 +7,8 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    int peca, movimento,quantidade,opcao,opcao2;
+    int peca, movimento,quantidade;
+    int opcao/*peça*/,opcao2/*jogar novamente*/,opcao3/*lado*/;
 do{
     printf("*********************\n");
     printf("*Movimentação Xadrez*\n");
@@ -18,31 +19,88 @@ do{
     printf("2. Bispo\n");
     printf("3. Rainha\n");
     scanf("%d",&opcao);
-    
+    //opcao da peça
     switch(opcao)
     {   
         //torre
         case 1:
-        printf("Torre");
-        break;
-        //bispo
-        printf("bispo");
+        printf("Torre\n");
+        printf("para qual lado quer ir?\n");
+        printf("1. Direita\n");
+        printf("2.Esquerda\n");
+        printf("3.Cima\n");
+        printf("4.Baixo\n");
+        scanf("%d",&opcao3);
+        //lado que a torre vai
+        switch (opcao3)
+        {
+        // vai para direita
+        case 1:
+        printf("Direita\n");
+            break;
+        // vai para esquerda
         case 2:
+        printf("Esquerda\n");
+            break;
+        // vai para cima
+        case 3:
+        printf("Cima\n");
+            break;
+        // vai para baixa
+        case 4:
+        printf("baixo\n");
+            break;
+
+        default:
+        printf("Opção invalida!\n");
+            break;
+        }
         break;
+
+        //bispo
+        case 2:
+        printf("bispo\n");
+        printf("para qual lado quer ir?\n");
+        printf("1. Direita\n");
+        printf("2.Esquerda\n");
+        printf("3.Cima\n");
+        printf("4.Baixo\n");
+        scanf("%d",&opcao3);
+        
+        //lado que a Bispo vai
+
+        break;
+
         //rainha
         case 3:
-        printf("rainha");
+        printf("rainha\n");
+        printf("para qual lado quer ir?\n");
+        printf("1. Direita\n");
+        printf("2.Esquerda\n");
+        printf("3.Cima\n");
+        printf("4.Baixo\n");
+        scanf("%d",&opcao3);
+        
+        //lado que a Rainha vai
+
+
         break;
 
         default:
-        printf("Opção invalida!");
+        printf("Opção invalida!\n");
         break;
 
+    
     }
-    printf("Voltar para o inicio Digite 1\n");
-    printf("Para finalizar o jogo Digite 2\n");
+
+    // jogar novamente ou sair
+    printf("Esolha uma opção:\n");
+    printf("1. Jogar novamente\n");
+    printf("2. Sair do jogo\n");
+    scanf("%d",&opcao2);
 }while(opcao2 == 1);
-    printf("Jogo Finalizado");
+    printf("Jogo Finalizado\n");
+
     
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
